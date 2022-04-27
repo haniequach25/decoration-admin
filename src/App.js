49ebbf429b0a0f35ec2ed01e2c63a4c23b944c50
haniequach25 <lenovoa60quach@gmail.com>
2022-04-28 00:50:43 +0700

    update repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Menu } from 'antd';
+import LayoutAdmin from 'features/LayoutAdmin';
+import LoginForm from 'features/Login';
+import { getMe } from 'features/Login/loginSlice';
+import { createBrowserHistory } from 'history';
+import { useEffect, useState } from 'react';
+import React from 'react';
+import useMetaTags from 'react-metatags-hook';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 function App() {
+  const [collapsed, setCollapsed] = useState(true);
+  const [selectedKeys,setSelectedKeys] = useState(1);
+  const onCollapse = () => {
+    const setCl = !collapsed;
+    setCollapsed(setCl);
+  };
+  const dispatch = useDispatch();
+  useMetaTags({
+    title: "LMG Admin"
+  })
+
+  const user = useSelector((state) => state.me.user);
+
+  const token = useSelector((state) => state.me.token);
+  const route = (
+    <Switch>
+      <Route path="/login" component={LoginForm} />
+      {token == "" ? <Redirect to="/login" /> : <Redirect to="/" />}
+    </Switch>
+  );
+
+  useEffect(() => {
+    const action = getMe();
+    dispatch(action);
+  }, []);
+  const history = createBrowserHistory({ forceRefresh: true });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      {token == "" ? (
+        route
+      ) : (
+      <LayoutAdmin />
+      )}
+    </Router>
   );
 }
 
-export default App;
+export default App // instead of "export default App;"
